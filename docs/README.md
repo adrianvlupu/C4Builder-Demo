1278168c33e087e338db8821c5038d4d99cec7e3
Adrian Lupu <adrianvlupu@gmail.com>
2021-01-09 21:20:25 +0200

    custom docsify template example

--- docs/README.md
+++ docs/README.md
@@ -10,7 +10,7 @@
 
 ---
 
-![diagram](https://www.plantuml.com/plantuml/svg/0/RL9BRnen4BuZyH-cdAX4s4jFEIMXgpOIj2Y0efpGh1d0YXyhppYGVr_R6r2sxN5cllaUOz-nO91epN3mHJjbuexWADBoRLK5F4qFMewnYKn1UIVaPAgyhN0NDBet4zjOpRxTB0qwsIpcbK4XbjJQpZBwBDEsK0y7y_kNtvlrzlbn_NCxht_KlxwFl-R-aW9xDsuRDO5HLw211tIlsXrW5bcyfJ1AmtjG7nNuFSYHe4ce2PoIGsWlcxa7g9IFJdWwIXghTnQoOzrm8dbChe8ZkIfqqy9lZ3_nLOH1FBnfEe5sUnyiYlOEiF5HihWEVQa9eDk1nLU25jyjzTMtzLd6b0tKDnQr0RxAhbCGNLmbzmkjWcU_5wZFweZkG42V5_wcj2NK0fF5W9R-asebFX036dEvcagDuZq3_yrspJ_vb4q2EaQLOQaYKSN8SF14fdkwxheR9iw3tCvfkrTTaTlnHuvyrGkYx9IZz135zMAzxC5iZ_ykuSk_-WC0)
+![diagram](context.png)
 
 **Level 1: System Context diagram**
 
--- docsify.template.js
+++ docsify.template.js
@@ -1,34 +1,37 @@
-<!DOCTYPE html>
+// {
+//     name: '{{name}}',
+//     repo: '',
+//     loadSidebar: true,
+//     auto2top: true,
+//     homepage: 'index.md',
+//     plantuml: {
+//       skin: 'classic'
+//     },
+//     stylesheet: ''
+//   }
+module.exports = options => {
+  return `<!DOCTYPE html>
     <html lang="en">
     
     <head>
       <meta charset="UTF-8">
-      <title>Custom Template for me</title>
+      <title>Custom Template example</title>
       <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
       <meta name="description" content="Description">
       <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-      <link rel="stylesheet" href="//unpkg.com/docsify/lib/themes/vue.css">
+      <link rel="stylesheet" href="${options.stylesheet}">
     </head>
     
     <body>
       <div id="app"></div>
       <script>
-        window.$docsify = {
-  "name": "C4Builder-Demo",
-  "repo": "",
-  "loadSidebar": true,
-  "auto2top": true,
-  "homepage": "HOME.md",
-  "plantuml": {
-    "skin": "classic"
-  },
-  "stylesheet": "//unpkg.com/docsify/lib/themes/vue.css"
-};
+        window.$docsify = ${JSON.stringify(options, null, 2)};
       </script>
       <script src="//unpkg.com/docsify/lib/docsify.min.js"></script>
       <script src="//unpkg.com/docsify-plantuml/dist/docsify-plantuml.min.js"></script>
       <script src="//cdn.jsdelivr.net/npm/docsify/lib/plugins/zoom-image.min.js"></script>
     </body>
     
-    </html>
+    </html>`;
+};
